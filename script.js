ea8df2df35f7b706caa13df25a99741fb9137ff2
Maxwell Moura <maxwellcmoura@gmail.com>
2025-06-23 18:14:20 -0300

    Subindo Versão 2.0b

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 document.getElementById('btnConfirmar').addEventListener('click', function () {
   const nome = document.getElementById('nome').value.trim();
- 
 
   if (!nome) {
-    alert("Por favor, preencha todos os campos.");
+    alert("Por favor, preencha seu nome.");
     return;
   }
 
-  const numero = '5588999760102'; 
+  const numero = '+5588999760102'; // Altere para seu número real com DDI e DDD
   const mensagem = `Olá! Aqui é ${nome}. Confirmo presença no aniversário da Emylly 🎉`;
 
   const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
 
- 
   window.location.href = url;
 });
